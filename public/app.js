6c97f6b182d93551cfded4b7437487cbb9451698
user <user@codeclan013.home>
2017-06-24 18:42:33 +0100

    added drop down list of all unique crimes

--- public/app.js
+++ public/app.js
@@ -2,66 +2,61 @@
 var app = function(){
 
   var crimeList = [];
-  var selectedCrimes = JSON.parse(localStorage.getItem('crimes')) || {}
+  var selectedCrimes = JSON.parse( localStorage.getItem( 'crimes' )) || {}
 
   var url = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2016-12";
 
   var request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.addEventListener('load', function() {
+  request.open( "GET", url );
+  request.addEventListener( 'load', function() {
     var jsonString = this.responseText;
-    var crimes = JSON.parse(jsonString);
-    console.log("aaaaaaaaa",crimes);
+    var crimes = JSON.parse( jsonString );
 
-    render( crimes );
+    var chosenCrime = render( crimes );
+    populateSelect ( crimes );
+
   });
   request.send();
 }
 
 //render 
 var render = function( crimes ) {
-  var div = document.querySelector('#main');
-  console.log("crimmmmmmmmmmme", crimes)
-
-  var storedCrime = localStorage.getItem('crimes');
+  var div = document.querySelector( '#main' );
+  var storedCrime = localStorage.getItem( 'crimes' );
   var crimeToDisplay = null;
 
-  if (storedCrime) {
+  if ( storedCrime ) {
     crimeToDisplay = JSON.parse( storedCrime );
-    var select = document.querySelector('#crime-list');
+    var select = document.querySelector( '#crime-list' );
     select.selectedIndex = crimeToDisplay.index;
   }
   else{
     crimeToDisplay = crimes[0];
   }
-  populateSelect(crimes);
-  console.log("chosencrime", chosenCrime)
-  console.log('crimmmmmes', crimes)
+  return crimeToDisplay;  console.log('crimmmmmes', crimes)
 
-
-  do a onclick here *******************
-  // displayCrimes( chosenCrime, crimes );
 }
 
 
 // displayCrimes
-var displayCrimes = function(chosenCrime, allCrimes) {
-  var pTags = document.querySelectorAll('#main p');
-  console.log("chosenCrime", chosenCrime);
-  console.log("all crimes",allCrimes)
+// var displayCrimes = function(chosenCrime, allCrimes) {
+//   var pTags = document.querySelectorAll('#main p');
+//   console.log("chosenCrime", chosenCrime);
+//   console.log("all crimes",allCrimes)
 
-  allCrimes.forEach(function(item, index) {
-    if (allCrimes[index] = chosenCrime) {
-      pTags[0].innerText = allCrimes[index].street_name
-    }
-    allCrimes[index] = item.category;
-  });
+//   allCrimes.forEach(function(item, index) {
+//     if (allCrimes[index] = chosenCrime) {
+//       pTags[0].innerText = allCrimes[index].street_name
+//     }
+//     allCrimes[index] = item.category;
+//   });
 
 
-}
+// }
 
 //populateSelect
 var populateSelect = function( crimes ) {
+  console.log("populateselect")
   var crimeList = [];
   var uniqueCrimeList= [];
   var select = document.querySelector('#crime-list');
@@ -71,13 +66,26 @@ var populateSelect = function( crimes ) {
   });
 
   var uniqueCrimeList = removeDuplicates( crimeList );
-
   uniqueCrimeList.forEach (function( uniqueCrimeList, index ) {
     var option = document.createElement('option');
     option.text = uniqueCrimeList;
     select.appendChild(option);  
   });
 
+  var div = document.querySelector( '#main p' );
+  console.log('div', div)
+  div.addEventListener('change', function ( event ) {
+    console.log(" in event listerne")
+    var index = this.value;
+    var crime = uniqueCrimeList[index];  
+
+    console.log(" MAKESELECTION index", index);
+    console.log(" MAKESELECTION crime", crime);
+    updateInfor(crime);
+
+    var jsonString = JSON.stringify( crime );
+    localStorage.setItem( 'selectedCrime', jsonString )  
+  })
 }
 
 //removeDuplicates
@@ -89,31 +97,30 @@ var removeDuplicates = function ( crimeList ) {
   return  uniqueCrimeList;
 }
 
-//   brewdogBeers.forEach(function(item, index) {
-//     item.index = index;
+//makesSelection
+// var makeSelection = function( uniqueCrimeList, crimes ) {
+//   console.log("makeselection")
+//   var div = document.querySelector( '#main p' )
 
-//     var option = document.createElement('option');
-//     option.value = index;
-//     option.text = item.name;
-//     select.appendChild(option);
-//   });
-
-//   select.addEventListener('change', function(event) {
+//   div.addEventListener('change', function ( event ) {
+//     console.log(" in event listerne")
 //     var index = this.value;
-//     var beer = brewdogBeers[index];
+//     var crime = uniqueCrimeList[index];  
 
-//     console.log("index", index);
-//     console.log("beer", beer)
+//     console.log(" MAKESELECTION index", index);
+//     console.log(" MAKESELECTION crime", crime);
+//     updateInfor(crime);
 
-//     updateInfo(beer);
+//     var jsonString = JSON.stringify( crime );
+//     localStorage.setItem( 'selectedCrime', jsonString )  
 
-//     var jsonString = JSON.stringify(beer);
-  //   localStorage.setItem('selectedBeer' , jsonString);
-  // })
-
-
-
-
+//     crimes.forEach(function(item, index) {
+//         if ( crimes[index] = chosenCrime) {
+//           pTags[0].innerText = allCrimes[index].street_name
+//         }
+//         allCrimes[index] = item.category;
+//       });
+//   })
 
 
   window.addEventListener('load', app);
